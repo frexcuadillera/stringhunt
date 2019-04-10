47924c78387278222c837b0c2f02e15e92f2dc46
frexcuadillera <fcuadillera@gmail.com>
2019-04-10 22:27:51 +0800

    implement game attack

--- src/stringhunt/states/GameState.java
+++ src/stringhunt/states/GameState.java
@@ -1,27 +1,64 @@
 package stringhunt.states;
 
 import javax.swing.*;
+
+import stringhunt.gfx.Assets;
+import stringhunt.letter.LetterPanel;
+
+import java.awt.event.*;
 import java.awt.*;
 
-public class GameState {
+public class GameState implements ActionListener {
     
     private JPanel gamePanel;
-    private JLabel testlabel;
+    private LetterPanel letterPanel;
+    
+    private JButton attackButton;
+    
+    private final int X = 300;
+    private final int Y = 350;
+    private final int WIDTH = 200;
+    private final int HEIGHT = 50;
+    
+    private final int LETTER_PANEL_X = 10;
+    private final int LETTER_PANEL_Y = 10;
+    
     
     public GameState() {
 	gamePanel = new JPanel();
-	
-	testlabel = new JLabel("game frame");
-	
+	gamePanel.setLayout(null);
 	gamePanel.setBounds(0, 0, 800, 450);
+		
+	letterPanel = new LetterPanel(LETTER_PANEL_X, LETTER_PANEL_Y);
 	
-	testlabel.setLocation(10, 10);
-	gamePanel.add(testlabel);
+	attackButton = new JButton();
+	attackButton.addActionListener(this);
+	attackButton.setBounds(X, Y, WIDTH, HEIGHT);
+	attackButton.setIcon(new ImageIcon(Assets.btn_attack[0]));
+
+	gamePanel.add(attackButton);
 	System.out.println("entered game frame");
+    }
+    
+    public void tick() {
+	System.out.println("game state ticking");
     }
     
     public JPanel getGamePanel() {
         return gamePanel;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent e) {
+	
+	if(e.getSource() == attackButton) {
+	    System.out.println("attack pressed");
+	    //attack event
+	    
+	    //update board
+	    
+	}
+	
     }
 
 }
