6a682db7f0381a35ab233e781aaab017d61f972e
frexcuadillera <fcuadillera@gmail.com>
2019-04-12 03:52:32 +0800

    Create ScenePanel.java

---
+++ src/stringhunt/gamescene/ScenePanel.java
@@ -0,0 +1,11 @@
+package stringhunt.gamescene;
+
+import javax.swing.JPanel;
+
+public class ScenePanel extends JPanel {
+    
+    public ScenePanel() {
+	System.out.println("scenepanel added");
+    }
+
+}
