102fc194d93e3155e04a154eb213a537a43b89cc
frexcuadillera <fcuadillera@gmail.com>
2019-04-13 00:54:47 +0800

    added scene panel

--- src/stringhunt/states/GameState.java
+++ src/stringhunt/states/GameState.java
@@ -1,36 +1,53 @@
 package stringhunt.states;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.Arrays;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
 import stringhunt.StringHunt;
 import stringhunt.gfx.Assets;
+import stringhunt.letter.LetterDamageCalculator;
 import stringhunt.letter.LetterPanel;
 
 public class GameState implements ActionListener {
    
-    private JPanel gamePanel;
-    private LetterPanel letterPanelConstructor;
-    private JPanel letterPanelObject;
+    private JPanel gamePanel;   
+    private JPanel letterPanelObject;   
+    private JPanel scenePanel;
+    
+    private JLabel playerHealthLabel;
+    private JLabel enemyHealthLabel;
+    
     public static JTextField textField;
     
     private JButton resetButton;
     private JButton attackButton;
     private JButton menuButton;
     
+    private LetterPanel letterPanelConstructor;
+    private LetterDamageCalculator ldc;
+    
     private final int X = 300;
     private final int Y = 500;
     private final int WIDTH = 200;
     private final int HEIGHT = 50;
     
-    private final int LETTER_PANEL_OBJECT_X = 105;
+    private final int LETTER_PANEL_OBJECT_X = 100;
     private final int LETTER_PANEL_OBJECT_Y = 280;
+    private final int LETTER_PANEL_WIDTH = 600;
+    private final int LETTER_PANEL_HEIGHT = 170;
+    
+    private final int SCENE_PANEL_OBJECT_X = 100;
+    private final int SCENE_PANEL_OBJECT_Y = 50;
+    private final int SCENE_PANEL_WIDTH = 600;
+    private final int SCENE_PANEL_HEIGHT = 150;
     
     private final int TEXT_FIELD_COLUMNS = 20;
     private final int TEXT_FIELD_X = 300;
@@ -38,6 +55,19 @@ public class GameState implements ActionListener {
     private final int TEXT_FIELD_WIDTH = 200;
     private final int TEXT_FIELD_HEIGHT = 30;
     
+    private final int HEALTH_WIDTH = 50;
+    private final int HEALTH_HEIGHT = 10;
+    
+    private final int PLAYER_HEALTH_X = 50;
+    private final int PLAYER_HEALTH_Y = 10;
+    
+    private final int ENEMY_HEALTH_X = 500;
+    private final int ENEMY_HEALTH_Y = 10;
+    
+    private int currentPlayerHealth = 10;
+    private int currentEnemyHealth = 10;
+    
+    private int currentEnemy = 1;
     private int currentLevel = 1;
        
     public GameState() {
@@ -48,12 +78,63 @@ public class GameState implements ActionListener {
 	//letter panel
 	letterPanelConstructor = new LetterPanel();
 	letterPanelObject = letterPanelConstructor.getLetterPanel();	
-	letterPanelObject.setBounds(LETTER_PANEL_OBJECT_X, LETTER_PANEL_OBJECT_Y, 590, 170);
+	letterPanelObject.setBounds(
+		LETTER_PANEL_OBJECT_X, 
+		LETTER_PANEL_OBJECT_Y, 
+		LETTER_PANEL_WIDTH, 
+		LETTER_PANEL_HEIGHT
+	);
+	
+	//scene panel
+	scenePanel = new JPanel();
+	scenePanel.setLayout(null);
+	scenePanel.setBounds(
+		SCENE_PANEL_OBJECT_X, 
+		SCENE_PANEL_OBJECT_Y, 
+		SCENE_PANEL_WIDTH, 
+		SCENE_PANEL_HEIGHT
+	);	
+	scenePanel.setBackground(new Color(0,255,0));
+	
+	//player health
+	playerHealthLabel = new JLabel(String.valueOf(currentPlayerHealth)+"/10 HP");
+	playerHealthLabel.setBounds(
+		PLAYER_HEALTH_X, 
+		PLAYER_HEALTH_Y, 
+		HEALTH_WIDTH, 
+		HEALTH_HEIGHT
+	);
+	scenePanel.add(playerHealthLabel);
+	
+	//enemy health
+	enemyHealthLabel = new JLabel(
+		String.valueOf(currentEnemyHealth) +
+		"/" +
+		(10 + ((currentEnemy - 1) * 5)) +
+		" HP"
+	);
+	enemyHealthLabel.setBounds(
+		ENEMY_HEALTH_X, 
+		ENEMY_HEALTH_Y,
+		HEALTH_WIDTH, 
+		HEALTH_HEIGHT
+	);
+	scenePanel.add(enemyHealthLabel);
 	
 	//text field
 	textField = new JTextField(TEXT_FIELD_COLUMNS);
-	textField.setBounds(TEXT_FIELD_X,TEXT_FIELD_Y,TEXT_FIELD_WIDTH,TEXT_FIELD_HEIGHT);
+	textField.setBounds(
+		TEXT_FIELD_X,
+		TEXT_FIELD_Y,
+		TEXT_FIELD_WIDTH,
+		TEXT_FIELD_HEIGHT
+	);
 	textField.setEditable(false);
+	textField.addActionListener(this);
+	textField.setText(null);
+	
+	//letter damage calculator
+	ldc = new LetterDamageCalculator();
 	
 	//reset button
 	resetButton = new JButton();
@@ -69,7 +150,7 @@ public class GameState implements ActionListener {
 	attackButton.setIcon(new ImageIcon(Assets.btn_attack[0]));
 	attackButton.setPressedIcon(new ImageIcon(Assets.btn_attack[1]));
 	
-	//menubutton
+	//menu button
 	menuButton = new JButton();
 	menuButton.addActionListener(this);
 	menuButton.setBounds(X + 250, Y, WIDTH, HEIGHT);
@@ -77,6 +158,7 @@ public class GameState implements ActionListener {
 	menuButton.setPressedIcon(new ImageIcon(Assets.btn_menu[1]));
 
 	//add to game panel
+	gamePanel.add(scenePanel);
 	gamePanel.add(letterPanelObject);
 	gamePanel.add(textField);
 	gamePanel.add(resetButton);
@@ -98,19 +180,49 @@ public class GameState implements ActionListener {
 	if(e.getSource() == resetButton) {
 	    System.out.println("reset pressed");
 	    textField.setText(null);
-	    letterPanelConstructor.resetBoard();
+	    letterPanelConstructor.resetBoard();	    
 	}
 	
-	if(e.getSource() == attackButton) {
-	    System.out.println("attack pressed");
-	    //attack event
+	if(e.getSource() == attackButton) {    
 	    
-	    Arrays.fill(letterPanelConstructor.alreadyPressed, false);
+	    //do nothing if text field is empty
+	    if(textField.getText().isEmpty()) {
+		return;
+	    }
 	    
-	    //update board
-	    //do validate entry here
+	   //do validate entry here
+	    if(ldc.validateWord(textField.getText())) {
+		currentEnemyHealth -= ldc.calculateDamageDealt(textField.getText());
+		System.out.println("word is valid, dealt " +ldc.calculateDamageDealt(textField.getText())+ " damage");
+	    } else {
+		currentPlayerHealth -= ldc.calculateDamageTaken(textField.getText());
+		System.out.println("word is invalid, received "+ldc.calculateDamageTaken(textField.getText())+ " damage");
+	    }
+	    
+	    //update health here
+	    playerHealthLabel.setText(String.valueOf(currentPlayerHealth)+"/10 HP");
+	    enemyHealthLabel.setText(		
+		    String.valueOf(currentEnemyHealth) +
+		    "/" +
+		    (10 + ((currentEnemy - 1) * 5)) +
+		    " HP"
+	    );
+	    
+	    //update board here
 	    textField.setText(null);
+	    Arrays.fill(letterPanelConstructor.alreadyPressed, false);
 	    letterPanelConstructor.updateBoard(currentLevel);
+	    
+	    if(currentPlayerHealth <= 0) {
+		//do game over event here
+		playerHealthLabel.setText("0/10 HP");
+		System.out.println("game over player health 0");
+	    }
+	    
+	    if(currentEnemyHealth <= 0) {
+		enemyHealthLabel.setText("0/10 HP");
+		System.out.println("enemy defeated");
+	    }
 	}
 	
 	if(e.getSource() == menuButton) {
