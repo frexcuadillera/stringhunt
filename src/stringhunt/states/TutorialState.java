1d6178fc054a80e5a6c6f0622afa26025f7e4e60
frexcuadillera <fcuadillera@gmail.com>
2019-04-18 23:01:03 +0800

    added bgcolor

--- src/stringhunt/states/TutorialState.java
+++ src/stringhunt/states/TutorialState.java
@@ -1,5 +1,6 @@
 package stringhunt.states;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
@@ -25,6 +26,7 @@ public class TutorialState implements ActionListener {
 	tutorialPanel = new JPanel();
 	tutorialPanel.setLayout(null);
 	tutorialPanel.setBounds(0 , 0, StringHunt.FRAME_WIDTH, StringHunt.FRAME_HEIGHT);
+	tutorialPanel.setBackground(Color.decode("#ACFFFF"));
 	
 	backButton = new JButton();		
 	backButton.setBounds(BACK_X, BACK_Y, BACK_WIDTH, BACK_HEIGHT);
