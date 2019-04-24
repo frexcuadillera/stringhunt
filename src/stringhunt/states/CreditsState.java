f9fc56535916341ddb9820d74c927895f513fcb6
frexcuadillera <fcuadillera@gmail.com>
2019-04-25 01:00:46 +0800

    finalize credit state

--- src/stringhunt/states/CreditsState.java
+++ src/stringhunt/states/CreditsState.java
@@ -6,6 +6,7 @@ import java.awt.event.ActionListener;
 
 import javax.swing.ImageIcon;
 import javax.swing.JButton;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 import stringhunt.StringHunt;
@@ -14,26 +15,32 @@ import stringhunt.gfx.Assets;
 public class CreditsState implements ActionListener{
 
     private JPanel creditsPanel;
+    private JLabel creditsLabel;
+    
     private JButton backButton;    
     private final int BACK_X = 20;
     private final int BACK_Y = 500;
     private final int BACK_WIDTH = 200;
     private final int BACK_HEIGHT = 50;
-
+       
     public CreditsState() {
 	
 	creditsPanel = new JPanel();
 	creditsPanel.setLayout(null);
 	creditsPanel.setBounds(0, 0, StringHunt.FRAME_WIDTH, StringHunt.FRAME_HEIGHT);
 	creditsPanel.setBackground(Color.decode("#ACFFFF"));
-	
+		
 	backButton = new JButton();	
 	backButton.setBounds(BACK_X, BACK_Y, BACK_WIDTH, BACK_HEIGHT);
 	backButton.setIcon(new ImageIcon(Assets.btn_back[0]));
 	backButton.setPressedIcon(new ImageIcon(Assets.btn_back[1]));
-
 	creditsPanel.add(backButton);
 	backButton.addActionListener(this);
+	
+	creditsLabel = new JLabel(new ImageIcon(Assets.credits_background));
+	creditsLabel.setBounds(0,0,800,600);
+	creditsPanel.add(creditsLabel);
+	
     }
     
     public JPanel getCreditsPanel() {
